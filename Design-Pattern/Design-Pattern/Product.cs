afe48210003ebf31ffc0eb6803b64b97ddb6f220
naogd <Deniz-dogan@t-online.de>
2024-12-11 16:03:18 +0100

    cleanup

--- Design-Pattern/Design-Pattern/Product.cs
+++ Design-Pattern/Design-Pattern/Product.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Design_Pattern;
 
 public enum ProductType
--- Design-Pattern/Design-Pattern/Program.cs
+++ Design-Pattern/Design-Pattern/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Design_Pattern;
 
 public static class Program
@@ -22,36 +24,78 @@ public static class Program
         
         // Bestellung verarbeiten
         baseOrder.ProcessOrder();
-
-        // 
-        S.Order order = new S.Order();
         
-        // // Produkte hinzufügen
-        order.AddProduct(skateboard);
-        order.AddProduct(schwarzbrot);
-        order.AddProduct(milch);
-        
-        // Zahlungsmethode und Benachrichtigung festlegen
-        S.NotificationService notificationService = new S.NotificationService("Email");
-        S.OrderProcessor orderProcessor = new S.OrderProcessor(notificationService, "PayPal");
-        
-        // Bestellung verarbeiten
-        orderProcessor.ProcessOrder(order);
-
-
+        //
+        // S.Order order = new S.Order();
+        //
+        // // // Produkte hinzufügen
+        // order.AddProduct(skateboard);
+        // order.AddProduct(schwarzbrot);
+        // order.AddProduct(milch);
+        //
+        // // Zahlungsmethode und Benachrichtigung festlegen
+        // S.NotificationService notificationService = new S.NotificationService("Email");
+        // S.OrderProcessor orderProcessor = new S.OrderProcessor(notificationService, "PayPal");
+        //
+        // // Bestellung verarbeiten
+        // orderProcessor.ProcessOrder(order);
+        //
+        //
         O.Order oOder = new O.Order();
         
         // // Produkte hinzufügen
         oOder.AddProduct(skateboard);
         oOder.AddProduct(schwarzbrot);
         oOder.AddProduct(milch);
-
+        
         O.NotificationService oNotificationService = new O.NotificationService("Email");
         O.Interfaces.IPaymentProcessor paypalPayment = new O.PaymentModel.PayPalPayment();
         O.OrderProcessor oOrderProcessor = new O.OrderProcessor(oNotificationService, paypalPayment);
         
         oOrderProcessor.ProcessOrder(oOder);
-
+        
+        
     }
 }
 
+/*
+Calculator c = new Calculator();
+IShape r = new Reactangle { H = 2, W = 2 };
+IShape cc = new Circle { R = 1 };
+Console.WriteLine(c.CalcArea(r));
+Console.WriteLine(c.CalcArea(cc));
+public interface IShape
+{ 
+    double Calc();
+}
+
+public class Reactangle : IShape
+{
+    public double W { get; init; }
+    public double H { get; init; }
+
+    /// <inheritdoc />
+    public double Calc()
+    {
+        return W * H;
+    }
+}
+public class Circle : IShape
+{
+    public double R { get; set; }
+
+    /// <inheritdoc />
+    public double Calc()
+    {
+        return Math.Pow(R, 2) * Math.PI;
+    }
+}
+
+public class Calculator
+{
+    public double CalcArea(IShape r)
+    {
+        return r.Calc();
+    }
+}
+*/
